bf6e86e75c256bd4fa96b9216450398d925fc2a8
tjohnson <tjohnson@axway.com>
2018-12-01 23:20:25 -0700

    pagination

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,11 +2,8 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
 	items: forwardTo('db'),
-	item: forwardTo('db')
-	// async items(parent, args, ctx, info) {
-	// 	const items = await ctx.db.query.items();
-	// 	return items;
-	// }
+	item: forwardTo('db'),
+	itemsConnection: forwardTo('db')
 };
 
 module.exports = Query;
